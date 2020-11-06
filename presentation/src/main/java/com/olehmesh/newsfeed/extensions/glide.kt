0ce119505d011a4e416bd538380e506e66e4f504
Oleh M <oleh.mesh@gmail.com>
2020-11-06 14:44:39 +0200

    replacing circle image to rectangle in DetailFragment

--- presentation/src/main/java/com/olehmesh/newsfeed/extensions/glide.kt
+++ presentation/src/main/java/com/olehmesh/newsfeed/extensions/glide.kt
@@ -1,6 +1,8 @@
 package com.olehmesh.newsfeed.extensions
 
 import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -22,12 +24,12 @@ fun CircleImageView.setLogo(
 
 }
 
-fun CircleImageView.setImageDetail(imgDetailURL: String?) {
+fun AppCompatImageView.setImageDetail(imgDetailURL: String?) {
         Glide.with(this).asBitmap()
                 .load(imgDetailURL)
                 .apply(RequestOptions().encodeQuality(100))
                 .placeholder(R.drawable.ic_placeholder)
                 .error(R.drawable.ic_placeholder)
-            .into(this)
+                .into(this)
 
 }
