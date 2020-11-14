e6fb5f476dd7aaf1becdecb44f6a6563779b0623
Oleh Meshcheriakov <omeshcheriakov@gbsfo.com>
2020-11-14 12:51:36 +0200

    delete unnecessary check exception

--- presentation/src/main/java/com/olehmesh/newsfeed/base/BaseFragment.kt
+++ presentation/src/main/java/com/olehmesh/newsfeed/base/BaseFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
